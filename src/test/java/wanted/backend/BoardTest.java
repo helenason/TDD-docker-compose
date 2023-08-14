144e667998ab01e16d500dd460836b8a49232db4
SCY <helenason@naver.com>
2023-08-15 02:37:26 +0900

    test: 게시판 목록 조회 결과 없을 경우 테스트

--- src/test/java/wanted/backend/BoardTest.java
+++ src/test/java/wanted/backend/BoardTest.java
@@ -108,4 +108,32 @@ public class BoardTest {
             Assertions.assertEquals(data.get(i).getTitle(), "제목" + (root - i));
         }
     }
+
+    @Test
+    @DisplayName("게시판 목록 조회 - 결과 없음")
+    void listEmpty() {
+
+        // given
+        Member member = memberRepository.save(
+                Member.builder()
+                        .email("test@gmail.com")
+                        .password("test123!")
+                        .build());
+
+        int total_size = 10;
+        for (int i = 0; i < total_size; i ++) {
+            BoardDto.Request boardDto = new BoardDto.Request("제목" + (i + 1), "내용" + (i + 1));
+            boardService.writePost(boardDto, member);
+        }
+
+        // when
+        int page = 1000; int size = 5;
+        ResponseDto responseDto = boardService.listPosts(page);
+
+        // then
+        List<BoardListDto> data = (List<BoardListDto>) responseDto.getData();
+
+        Assertions.assertEquals(responseDto.getStatus(), HttpStatus.OK);
+        Assertions.assertEquals(data.size(), 0);
+    }
 }
